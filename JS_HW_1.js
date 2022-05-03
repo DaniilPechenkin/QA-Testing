4c52a0d931a359838dd3c740f29709c89261d77f
DaniilPechenkin <daniilps96@gmail.com>
2022-05-03 13:53:52 +0500

    JS_1 Homework

---
+++ JS_HW_1.js
@@ -0,0 +1,41 @@
+let item1;
+item1 = 5;
+console.log("Задание 3", item1);
+
+let item2;
+item2 = 3;
+console.log("Задание 6", item2);
+
+let item3;
+item3 = item1 + item2;
+console.log("Задание 9", item3);
+
+let item4;
+item4 = "yolochka";
+
+console.log("Задание 12", item4);
+console.log("Задание 13", item3 + item4);
+console.log("Задание 14", item3 * item4);
+
+let item5;
+item5 = item3;
+let item6; let item6_type
+item6 = 15;
+item6_type = typeof(item6);
+console.log("Задание 21", "item6 = "+ item6, "item6_type = "+ item6_type);
+
+let item7 = String(item6);
+let item7_type = typeof(item7);
+console.log("Задание 25","item7 = "+ item7, "item7_type = "+ item7_type);
+
+let age1 = 10;
+let age2 = 18;
+let age3 = 60;
+if (age1 == 10) {console.log("Задание 29")}
+if (age1 < age2 + age1) {
+    console.log("Задание 30 You don’t have access cause your age is "+ age1, ". It’s less then "+ age2)
+    }
+if (age1 >= age2 & age1 < age3) console.log("Задание 31 = Welcome!")
+if (age1 > age3) console.log("Keep calm and look Culture channel")
+else {console.log("Technikal work")}
+
